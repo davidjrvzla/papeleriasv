fc49a231982c43f6049ad6938cd423683de82d1b
davidjrvzla <65772388+davidjrvzla@users.noreply.github.com>
2020-07-11 13:06:36 -0400

    Update delcategory-view.php

--- core/app/view/delcategory-view.php
+++ core/app/view/delcategory-view.php
@@ -1,6 +1,10 @@
 <?php
 
-$category = Categorydata::getById($_GET["id"]);
+$category = CategoryData::getById($_GET["id"]);
+$products = ProductData::getAllByCategoryId($category->id);
+foreach ($products as $product) {
+	$product->del_category();
+}
 $category->del();
 Core::redir("./index.php?view=categories");
 
